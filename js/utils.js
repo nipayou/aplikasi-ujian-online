04755b1817a1f78efb0a0b945aaa1759ab9965f6
nipayou <bellasurtini0@gmail.com>
2026-09-06 21:26:50 +0700

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,20 @@
+// Utilitas Pembantu Global
+const Utils = {
+  // Format waktu durasi (detik ke HH:MM:SS)
+  formatTimer(totalSeconds) {
+    const hrs = Math.floor(totalSeconds / 3600);
+    const mins = Math.floor((totalSeconds % 3600) / 60);
+    const secs = totalSeconds % 60;
+    return `${String(hrs).padStart(2, '0')}:${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+  },
+
+  // Algoritma Fisher-Yates untuk acak array tanpa bias
+  shuffleArray(array) {
+    const arr = [...array];
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+};
