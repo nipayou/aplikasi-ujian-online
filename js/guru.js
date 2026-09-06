92fe803441bb0ecd938bf46feeea7a55a1894f42
nipayou <bellasurtini0@gmail.com>
2026-09-06 21:24:16 +0700

    Create guru.js

---
+++ js/guru.js
@@ -0,0 +1,7 @@
+// Modul Pengelolaan Dashboard Guru
+// Implementasi lengkap pada Checkpoint 12 - 15
+const GuruModule = {
+  initDashboard() {
+    console.log("Dashboard guru diinisialisasi.");
+  }
+};
