74069148b88a7934500b86eec1829a47a439127e
nipayou <bellasurtini0@gmail.com>
2026-09-06 21:25:20 +0700

    Create ujian.js

---
+++ js/ujian.js
@@ -0,0 +1,7 @@
+// Engine Lembar Ujian, Autosave, & Anti-Curang
+// Implementasi lengkap pada Checkpoint 29 - 40
+const UjianModule = {
+  initUjian() {
+    console.log("Engine ujian siap.");
+  }
+};
